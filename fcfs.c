f0921b39bcb18c2f944a670dd0b655704a962b68
Akari294 <anjliiilaald2@gmail.com>
2024-08-04 18:31:15 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,1 +1,28 @@
-
+#include <stdio.h>
+void main()
+{
+   int tat[20],bt[20],wt[20],n,i,twt,ttat;
+   printf("enter total number of processes");
+   scanf("%d",&n);
+   printf("enter the burst time of each processes");
+   for(i=1;i<=n;i++)
+{ 
+   printf("\np%d:",i);
+   scanf("%d",&bt[i]);
+}
+wt[1]=0;
+twt=0;
+ttat=0;
+for(i=1;i<=n;i++)
+{
+ tat[i]=wt[i+1]=wt[i]+bt[i];
+ twt+=wt[i];
+ ttat+=tat[i];
+}
+printf("\nprocess\t\tburst time\twaiting time\tturnaround time");
+for(i=1;i<=n;i++)
+{
+printf("\np%d\t\t%d\t\t%d\t\t%d",i,bt[i],wt[i],tat[i]);
+printf("\navg wait time:%f",(float)/twt/n);
+printf("\navg turn time:%f",(float)/ttat/n);
+}
